2c6a4b45ddb9b775402b0866048dc315c4c16f1a
Vyacheslav Khoroshev <vyacheslavkhoroshev.gmail.com>
2025-05-03 14:23:38 +0300

    Add tabs #20

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
-console.log("main.js виконується до завантаження HTML");
+const mainTabButtons = document.querySelectorAll(
+  'button[data-tab-type="primary"]'
+);
+const mainTabPanels = document.querySelectorAll('div[data-tab-type="primary"]');
+const skillsTabButtons = document.querySelectorAll(
+  'button[data-tab-type="secondary"]'
+);
+const skillsTabPanels = document.querySelectorAll(
+  'div[data-tab-type="secondary"]'
+);
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("main.js виконується після завантаження HTML");
-});
+const setupTabs = (buttons, panels) => {
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const tabId = button.getAttribute("data-tab");
+
+      buttons.forEach((btn) => btn.classList.remove("active"));
+      panels.forEach((panel) => panel.classList.remove("active"));
+
+      button.classList.add("active");
+      document.getElementById(tabId).classList.add("active");
+
+      if (tabId === "skills") {
+        const isNotActive = !document.querySelector(".tabs__button.active");
+        if (isNotActive) {
+          buttons[0].classList.add("active");
+          panels[0].classList.add("active");
+        }
+      }
+    });
+  });
+};
+setupTabs(mainTabButtons, mainTabPanels);
+setupTabs(skillsTabButtons, skillsTabPanels);
